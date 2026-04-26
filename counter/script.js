40c8d54e53a79b6df03e8411b68a2e243210217f
Dipanjan Chowdhury <chowdhurydipanjan173@gmail.com>
2026-04-27 00:37:37 +0530

    Added colour change to negetive

--- counter/script.js
+++ counter/script.js
@@ -1,14 +1,30 @@
-let inc = document.querySelector("#inc")
-let dec = document.querySelector("#dec")
-let num=document.querySelector("#box h2")
+const inc = document.querySelector("#inc")
+const dec = document.querySelector("#dec")
+const num=document.querySelector("#box h2")
+const reset=document.querySelector("#reset")
 
 let count=0
+function render(){
+    
+    num.textContent=count
+    if(count<0){
+        num.style.color="red"
+    }
+    else{
+        num.style.color="white"
+    }
+}
 inc.addEventListener('click',function(){
     count=count+1;
-    num.innerHTML=count
+    render()
 })
 
 dec.addEventListener('click',function(){
     count=count-1;
-    num.innerHTML=count
+    render()
+})
+
+reset.addEventListener('click',function(){
+    count=0
+    render()
 })
